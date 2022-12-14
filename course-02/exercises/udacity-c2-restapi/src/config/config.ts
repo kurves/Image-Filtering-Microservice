726ed0dbacb4c7b1320c5d3d18605ac904ca273f
kurves <kavssimply@gmailcom>
2022-12-14 16:27:03 +0300

    fix: fix config.ts

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -7,25 +7,10 @@ export const config = {
     "dialect": "postgres",
     "aws_region":process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
- 
-    "jwt": {
-      "secret": " "
-},
-
-  "aws":{
-  "aws_region":process.env.AWS_REGION,
-  "aws_profile": process.env.AWS_PROFILE,
-  "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
-
+    "aws_media_bucket": process.env.AWS_BUCKET
+  },
+  "jwt": {
+    "secret": " "
   }
+ 
 }
-
-
-/*
-"prod": {
-  "username": "",
-  "password": "",
-  "database": "udagram_prod",
-  "host": "",
-  "dialect": "postgres"*/
